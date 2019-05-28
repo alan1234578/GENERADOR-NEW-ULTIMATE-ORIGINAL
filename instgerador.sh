5a61dfbea2262332aea2afb7e823b2eeebd1b2a0
alan1234578 <51100031+alan1234578@users.noreply.github.com>
2019-05-28 06:14:41 -0500

    Update instgerador.sh

--- instgerador.sh
+++ instgerador.sh
@@ -37,7 +37,7 @@ esac
 mv -f $HOME/$1 ${ARQ}/$1
 chmod +x ${ARQ}/$1
 }
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;33m--------------------------------------------------------------------\033[0m"
 meu_ip () {
 MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 MIP2=$(wget -qO- ipv4.icanhazip.com)
@@ -46,7 +46,7 @@ echo "$IP" > /usr/bin/vendor_code
 }
 meu_ip
 echo -e "\033[1;33mInstalando Arquivos... "
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;33m--------------------------------------------------------------------\033[0m"
 cd $HOME
 REQUEST=$(echo $SCPresq|$SUB_DOM)
 wget -O "$HOME/lista-arq" ${REQUEST}/GERADOR > /dev/null 2>&1
@@ -59,7 +59,7 @@ echo -e "\033[1;31m- \033[1;32mRecebido Com Sucesso!"
 [[ -e $HOME/$arqx ]] && veryfy_fun $arqx
 } || echo -e "\033[1;31m- \033[1;31mFalha (nao recebido!)"
 done
-[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/GENERADOR-NEW-ULTIMATE-ORIGINAL/master/Install/trans &> /dev/null
+[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://raw.githubusercontent.com/adm/GENERADOR-NEW-ULTIMATE-ORIGINAL/master/Install/trans &> /dev/null
 [[ -e /bin/http-server.py ]] && mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
 [[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || apt-get install bc -y &>/dev/null
 [[ $(dpkg --get-selections|grep -w "screen"|head -1) ]] || apt-get install screen -y &>/dev/null
@@ -72,15 +72,15 @@ service apache2 restart > /dev/null 2>&1 &
 IVAR2="/etc/key-gerador"
 echo "$Key" > $IVAR2
 rm $HOME/lista-arq
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;33m--------------------------------------------------------------------\033[0m"
 echo "/usr/bin/gerar.sh" > /usr/bin/gerar && chmod +x /usr/bin/gerar
 echo -e "\033[1;33m Perfeito, Use o Comando \033[1;31mgerar.sh o gerar \033[1;33mpara Gerenciar as Suas Keys e
  Atualizar a Base do servidor"
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;33m--------------------------------------------------------------------\033[0m"
 } || {
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;33m--------------------------------------------------------------------\033[0m"
 echo -e "\033[1;33mKey Invalida!"
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;33m--------------------------------------------------------------------\033[0m"
 }
 echo -ne "\033[0m"
 echo "qra-atsilK?65@%6087%?66d5K8888:%6+95+@@?+08" > /etc/key-gerador
